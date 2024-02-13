9bbc5d496da19d06ef7ed077af606a2329f00054
takanto <takato.kan@uni.minerva.edu>
2024-02-13 20:11:59 +0000

    cbam debug

--- models/cbam.py
+++ models/cbam.py
@@ -2,24 +2,37 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 class ChannelAttention(layers.Layer):
-    def __init__(self, channel, reduction=16):
+    def __init__(self, num_channels, reduction_ratio=16, pool_types=['avg', 'max']):
         super(ChannelAttention, self).__init__()
-        self.maxpool = layers.GlobalMaxPooling2D()
-        self.avgpool = layers.GlobalAveragePooling2D()
-        self.se = tf.keras.Sequential([
-            layers.Conv2D(channel // reduction, 1, use_bias=False),
-            layers.ReLU(),
-            layers.Conv2D(channel, 1, use_bias=False)
+        self.num_channels = num_channels
+        self.reduction_ratio = reduction_ratio
+        self.pool_types = pool_types
+
+    def build(self, input_shape):
+        self.mlp = tf.keras.Sequential([
+            layers.Dense(self.num_channels // self.reduction_ratio, activation='relu'),
+            layers.Dense(self.num_channels, activation='sigmoid')
         ])
-        self.sigmoid = layers.Activation('sigmoid')
 
     def call(self, x):
-        max_result = self.maxpool(x)
-        avg_result = self.avgpool(x)
-        max_out = self.se(tf.expand_dims(max_result, axis=1))
-        avg_out = self.se(tf.expand_dims(avg_result, axis=1))
-        output = self.sigmoid(max_out + avg_out)
-        return output
+        channel_att_sum = None
+        for pool_type in self.pool_types:
+            if pool_type == 'avg':
+                avg_pool = tf.reduce_mean(x, axis=[1, 2], keepdims=True)
+                avg_pool = tf.reshape(avg_pool, (tf.shape(avg_pool)[0], -1))
+                channel_att_raw = self.mlp(avg_pool)
+            elif pool_type == 'max':
+                max_pool = tf.reduce_max(x, axis=[1, 2], keepdims=True)
+                max_pool = tf.reshape(max_pool, (tf.shape(max_pool)[0], -1))
+                channel_att_raw = self.mlp(max_pool)
+            if channel_att_sum is None:
+                channel_att_sum = channel_att_raw
+            else:
+                channel_att_sum = channel_att_sum + channel_att_raw
+
+        scale = tf.expand_dims(tf.expand_dims(channel_att_sum, axis=1), axis=1)
+        scale = tf.broadcast_to(scale, x.shape)
+        return x * scale
     
 class EfficientChannelAttention(layers.Layer):
     def __init__(self, k_size=3, **kwargs):
@@ -38,22 +51,47 @@ class EfficientChannelAttention(layers.Layer):
         attn = tf.math.sigmoid(attn)
         attn = tf.expand_dims(attn, axis=1)
         x =  x * attn
-        x = tf.reshape(x, (-1, H*W, C))
         return x
+
+class BasicConv(layers.Layer):
+    def __init__(self, out_planes, kernel_size, stride=1, padding='same', dilation=1, groups=1, relu=True, bn=True, bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = layers.Conv2D(out_planes, kernel_size=kernel_size, strides=stride, padding=padding, dilation_rate=dilation, groups=groups, use_bias=bias)
+        self.bn = layers.BatchNormalization(epsilon=1e-5, momentum=0.01) if bn else None
+        self.relu = layers.ReLU() if relu else None
+
+    def call(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+
+class ChannelPool(layers.Layer):
+    def __init__(self):
+        super(ChannelPool, self).__init__()
+
+    def call(self, x):
+        max_pool = tf.reduce_max(x, axis=1, keepdims=True)
+        mean_pool = tf.reduce_mean(x, axis=1, keepdims=True)
+        return tf.concat([max_pool, mean_pool], axis=1)
 
 
 class SpatialAttention(layers.Layer):
     def __init__(self, kernel_size=7):
         super(SpatialAttention, self).__init__()
-        self.conv = layers.Conv2D(1, kernel_size, padding='same')
+        self.kernel_size = kernel_size
+        self.compress = ChannelPool()
+        self.spatial = BasicConv(1, kernel_size, padding='same', relu=False)
 
     def call(self, x):
-        max_result = tf.reduce_max(x, axis=3, keepdims=True)
-        avg_result = tf.reduce_mean(x, axis=3, keepdims=True)
-        result = tf.concat([max_result, avg_result], axis=3)
-        output = self.conv(result)
-        output = layers.Activation('sigmoid')(output)
-        return output
+        x_compress = self.compress(x)
+        x_out = self.spatial(x_compress)
+        scale = tf.sigmoid(x_out)  # Broadcasting
+        return x * scale
 
 
 class ChannelBlockAttentionBlock(layers.Layer):
