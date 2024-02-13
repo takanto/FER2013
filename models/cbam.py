3c3e63371a35614e9ed4d756deff724c78b0d552
takanto <takato.kan@uni.minerva.edu>
2024-02-13 21:51:01 +0000

    cbam debug

--- models/cbam.py
+++ models/cbam.py
@@ -9,21 +9,23 @@ class ChannelAttention(layers.Layer):
         self.pool_types = pool_types
 
     def build(self, input_shape):
+        num_channels = input_shape[-1]
         self.mlp = tf.keras.Sequential([
-            layers.Dense(self.num_channels // self.reduction_ratio, activation='relu'),
-            layers.Dense(self.num_channels, activation='sigmoid')
+            layers.Dense(num_channels // self.reduction_ratio, activation='relu'),
+            layers.Dense(num_channels, activation='sigmoid')
         ])
 
     def call(self, x):
+        _, H, W, C = x.shape
         channel_att_sum = None
         for pool_type in self.pool_types:
             if pool_type == 'avg':
                 avg_pool = tf.reduce_mean(x, axis=[1, 2], keepdims=True)
-                avg_pool = tf.reshape(avg_pool, (tf.shape(avg_pool)[0], -1))
+                #avg_pool = tf.reshape(avg_pool, (tf.shape(avg_pool)[0], -1))
                 channel_att_raw = self.mlp(avg_pool)
             elif pool_type == 'max':
                 max_pool = tf.reduce_max(x, axis=[1, 2], keepdims=True)
-                max_pool = tf.reshape(max_pool, (tf.shape(max_pool)[0], -1))
+                #max_pool = tf.reshape(max_pool, (tf.shape(max_pool)[0], -1))
                 channel_att_raw = self.mlp(max_pool)
             if channel_att_sum is None:
                 channel_att_sum = channel_att_raw
@@ -31,8 +33,9 @@ class ChannelAttention(layers.Layer):
                 channel_att_sum = channel_att_sum + channel_att_raw
 
         scale = tf.expand_dims(tf.expand_dims(channel_att_sum, axis=1), axis=1)
-        scale = tf.broadcast_to(scale, x.shape)
-        return x * scale
+        x = x * scale
+        x = tf.reshape(x, (-1, H, W, C))
+        return x
     
 class EfficientChannelAttention(layers.Layer):
     def __init__(self, k_size=3, **kwargs):
@@ -75,9 +78,9 @@ class ChannelPool(layers.Layer):
         super(ChannelPool, self).__init__()
 
     def call(self, x):
-        max_pool = tf.reduce_max(x, axis=1, keepdims=True)
-        mean_pool = tf.reduce_mean(x, axis=1, keepdims=True)
-        return tf.concat([max_pool, mean_pool], axis=1)
+        max_pool = tf.reduce_max(x, axis=3, keepdims=True)
+        mean_pool = tf.reduce_mean(x, axis=3, keepdims=True)
+        return tf.concat([max_pool, mean_pool], axis=3)
 
 
 class SpatialAttention(layers.Layer):
@@ -90,15 +93,21 @@ class SpatialAttention(layers.Layer):
     def call(self, x):
         x_compress = self.compress(x)
         x_out = self.spatial(x_compress)
-        scale = tf.sigmoid(x_out)  # Broadcasting
-        return x * scale
+        scale = tf.sigmoid(x_out)
+        x = x * scale
+        return x
 
 
 class ChannelBlockAttentionBlock(layers.Layer):
-    def __init__(self, channel=512, reduction=16, kernel_size=49):
+    def __init__(self, num_channels=512, reduction=16, kernel_size=49):
         super(ChannelBlockAttentionBlock, self).__init__()
-        self.ca = ChannelAttention(channel=channel, reduction=reduction)
-        self.sa = SpatialAttention(kernel_size=kernel_size)
+        self.num_channels = num_channels
+        self.reduction = reduction
+        self.kernel_size = kernel_size
+
+    def build(self, input_shape):
+        self.ca = ChannelAttention(num_channels=self.num_channels, reduction_ratio=self.reduction)
+        self.sa = SpatialAttention(kernel_size=self.kernel_size)
 
     def call(self, x):
         x_skip = x
